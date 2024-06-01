14eeedad133ea8cb92fc6b4878514c495fc77987
beniman8 <benik@live.ca>
2024-05-31 20:22:54 -0400

    variables and new math functions

---
+++ variables.py
@@ -0,0 +1,45 @@
+''' 
+This is how to use variables in order to store data
+
+variables are a simpe container for any kind of data
+
+variable gets a name  -----> variable  = 'data'  <------- data
+
+the equal sign is to assign the value of the data to the variable
+
+
+Variable naming conventions:
+
+variables can only containe letters , numbers and the underscore symbols
+
+use snake_case = 'snake case'
+variable names should make sense
+make sure variable names expresses it contents
+
+'''
+
+'In here we assign the result of this operation to the result variable'
+result = 5 + 7
+
+
+print(result)
+
+result2 = result / 2
+
+print('now we can use the result as a value and make more operations with it')
+
+print(result2)
+
+
+test_123_CAP_LETTERS = 'test'
+
+name = "jenny"
+
+#new math operation +=,-=,/=,*=
+#num1 = num1 + 5  is the same as num1+=5
+
+num1 = 12
+
+num1+=5
+
+print(num1)
