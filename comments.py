f359f17fa4aa3a30ad1db84871f76bf6bfe0e83e
beniman8 <benik@live.ca>
2024-06-01 04:20:11 -0400

    what and how to use comments in python

---
+++ comments.py
@@ -0,0 +1,23 @@
+'''
+comment don't influence the code 
+they just explain your code
+
+two way to write comments on python 
+
+the hashtag # anything after this symbole gets ignored by python but you can read it in your code
+
+the triple quotations mark
+'''
+
+test_variable = 'a realy important vale'#this is a really very important value!!!!!
+
+
+#############
+# a coment #
+#############
+
+
+'''
+another comment
+another_variable = 1234
+'''
