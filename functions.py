2ea4ded538166c7947e87d30e325c515615a17ee
beniman8 <benik@live.ca>
2024-05-31 20:44:16 -0400

    how functions work

---
+++ functions.py
@@ -0,0 +1,30 @@
+'''
+a function is called by adding brackets after the function name
+
+print()
+add()
+do_something()
+
+inside the brackets we can add arguments 
+
+that get passed down to the function
+'''
+
+#This function prints things in the terminal
+print('test')
+
+#this function tells you the length of the word
+len('this word')
+
+#we will not be able to see anything until we use the print function to see the value of len
+
+#now tjis will show us the length of this word
+print(len('this word'))
+
+
+#some function accepts mulptiple arguments
+print('first arg','second arg')
+
+find_max_vALUE = max(10,2,45)
+
+print(find_max_vALUE)
