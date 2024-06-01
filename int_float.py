204393a78ef8ea2779f6b266c6167550c194b437
beniman8 <benik@live.ca>
2024-06-01 05:44:32 -0400

    converting and playing with integers and floats numbers

---
+++ int_float.py
@@ -0,0 +1,13 @@
+
+print(type(1))
+print(type(1.2))
+
+#the int will tunr into a float automaticaly
+print(1 + 2.5)
+
+# Turn int into a float , we run a float function it conver int to float
+print(float(2))
+
+#turn float into int, it truncates or gets rid of anything after the floating point 6.9  = 6
+print(int(2.5))
+
