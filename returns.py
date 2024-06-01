1ec977cb44d793521e8eacbd3d3a9af932631e2d
beniman8 <benik@live.ca>
2024-06-01 04:13:38 -0400

    understanding returns ane what it does

---
+++ returns.py
@@ -0,0 +1,14 @@
+'''
+returns is the glue that connects pythons 
+
+example any operation returns value
+
+programming is a combination of  return values that returns different outputs
+
+'''
+#the upper makes the word capital the replace than replaces a to x len counts how many word inside of it paranthese()
+test_variable = len('A word'.upper().replace('A','X'))
+
+
+print(test_variable)
+
