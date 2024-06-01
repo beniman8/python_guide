9fa4f8086eba23523384fe5ba67129891499eb78
beniman8 <benik@live.ca>
2024-06-01 04:30:23 -0400

    understanding what python sees in order to execute a program

---
+++ lines_of_execution.py
@@ -0,0 +1,40 @@
+'''
+python does not see the empty lines in the code but you do
+
+this
+print()
+print()
+
+and this are the same for python
+print()
+
+print()
+
+
+python also ignores white space within a line
+print('this       will      not      read  the  space between')
+print(5+7               +2)
+
+pytho cares about indentation of an entire line
+
+    print() will give you an error
+
+you can't add space at the begining of a line
+
+
+
+we see physical lines and python sees a logical line
+print() print()  python needs to execute one line at a time so this willl give you an error
+
+to break physical line to logical line  use \
+
+a = 1+2+3\
+    2+3+8
+this will work
+
+or 
+print(); print()
+
+
+
+'''
