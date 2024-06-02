3b836da312bbfdeebca3b79a4883df69274c44f3
beniman8 <benik@live.ca>
2024-06-02 06:36:46 -0400

    understanding how to control the flow of your code

---
+++ flow_of_code.py
@@ -0,0 +1,24 @@
+'''
+pythom as may ways to control the flow of the code
+
+python use indentation to control the flow of the code.
+
+the indented lines belongs to the if statement
+if condition is True run:
+    line1
+    line2
+    line3
+line4
+line5
+
+
+4 ways to determine the fllow of the code
+
+if elif else run code if conditons are true
+match is if for more specific value
+while repeat while condition asr true 
+for run code every item in a container
+
+commands like break and next give you even mmore control
+
+'''
