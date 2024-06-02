9ee6f14390ddcad5212830fe4859b290824b3975
beniman8 <benik@live.ca>
2024-06-02 06:31:44 -0400

    what other datatype exist

---
+++ other_datatype.py
@@ -0,0 +1,17 @@
+'''
+
+python is flexible with datatypes
+
+
+function to change data types are
+
+int(),float(),str(),bool(),dict(),tuple(),set()
+
+
+None datatype is the absence of value
+
+sequence - range of numbers 
+
+bytes,comlex number, memoryvieew, frozensets etc..  you probably wont use them
+
+'''
