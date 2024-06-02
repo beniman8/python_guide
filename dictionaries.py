2edcedc90add3e4256aa5fb44e859fa5a3bdc1f9
beniman8 <benik@live.ca>
2024-06-01 20:55:18 -0400

    how to manipulate dictioaries

---
+++ dictionaries.py
@@ -0,0 +1,45 @@
+'''
+is a complexe container for other variables 
+
+{'key':'value',1:[1,2,3]}
+
+dicttionaries are nothing but organized container where every key gets a value
+
+think about it as a self storage unit that has a key for every unit that needs to be used 
+in order to acces the value or items that are inside the locker/ storage.
+
+you can not duplicate keys , once you add the same key you are overiding the old one
+
+indexing does not work with dictionary
+
+'''
+
+test_dict = {'car':123,'dog':['woof','meow'],1:True}
+
+print(test_dict['dog'][0])
+
+#basic of dictionary
+print(test_dict.values())
+print(test_dict.keys())
+print(test_dict.items())
+
+#converting dictionary
+
+print(list(test_dict))
+print(tuple(test_dict))
+print(str(test_dict))
+
+
+#indexing or return a value in the dictionary
+print(test_dict['car'])
+#get is a bit better because it doesnt crash when it does not get a key
+print(test_dict.get('car'))
+
+
+#Exercise 
+# do reasearch and use the update method to add another key value pair
+
+test_dict.update({6:'cat'})
+print(test_dict.items())
+
+
