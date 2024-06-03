f6c7628ff4dea5271aee710ee3553e6677362517
beniman8 <benik@live.ca>
2024-06-02 20:44:34 -0400

    while loop exercise

--- whileloop.py
+++ whileloop.py
@@ -5,3 +5,16 @@ while True:
     print('infinit looop)
 
 '''
+
+
+#exercise
+
+# use a while loop to create a list with only even values from 0 to 100
+
+x = 0
+
+while x <= 100: 
+    if x != 58:
+        if x % 2 == 0 or x == 0:     
+            print(x)
+    x +=1
