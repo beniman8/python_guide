95be89232bb8a4949fd3774dbaf67d73dd1dc634
beniman8 <benik@live.ca>
2024-06-05 00:20:08 -0400

    ternarary operation  how to shortened your code

---
+++ ternary_operator.py
@@ -0,0 +1,22 @@
+'''
+the different ways your code can be writen
+
+
+True value if expression else False value
+
+'''
+
+x=5 
+color=''
+
+if x <5:
+    color= 'blue'
+else:
+    color='red'
+    
+    
+#this is a ternary operator
+# i want the color green if x iis les than 7 else i want to color red
+color='green' if x <7 else 'red' 
+print(f"The color is {'green' if x <7 else 'red'}")
+a = ['green' if x <7 else 'red','yellow','orange' ]
