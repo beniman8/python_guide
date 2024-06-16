92b7907da96286cde51ac18a6b414c45ccd65f0d
beniman8 <benik@live.ca>
2024-06-16 09:40:39 -0400

    creating your own python modules and using it

---
+++ custom_modules.py
@@ -0,0 +1,10 @@
+import my_module
+
+print(my_module.test_var)
+
+my_module.test_func('dsds')
+
+classs =my_module.TEST()
+print(classs.name)
+
+print(my_module.sum_it(1,2,1))
---
+++ my_module.py
@@ -0,0 +1,16 @@
+test_var = ' test'
+
+
+def test_func(content):
+    print('COLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL')
+    
+    
+class TEST:
+    
+    def __init__(self) -> None:
+        
+        self.name = 'my app'
+        self.value= 3434
+        
+def sum_it(*args):
+    return sum(args)
