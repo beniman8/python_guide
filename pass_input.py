6f500d7ef2beba58c9a4fb4761b8cfd20a77c997
beniman8 <benik@live.ca>
2024-06-16 12:42:14 -0400

    how to use pass and input

---
+++ pass_input.py
@@ -0,0 +1,19 @@
+'''
+pass tells python to not do anything 
+
+you can add it to a function if you do not have the code yet
+
+input is a function that get user input
+
+it is also rarely use
+
+'''
+
+def really_important():
+    pass 
+
+
+user_input = input('Press a button\n')
+
+
+print(user_input)
